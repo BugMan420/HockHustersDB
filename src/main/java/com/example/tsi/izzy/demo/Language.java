d4961be11fafabb1212ef4c4ef1b6d9f2cc03713
BugMan420 <ismaelsim@hotmail.co.uk>
2022-02-10 14:10:37 +0000

    added tests to the LanguageTest class

--- src/main/java/com/example/tsi/izzy/demo/Language.java
+++ src/main/java/com/example/tsi/izzy/demo/Language.java
@@ -34,7 +34,6 @@ public class Language {
         return language_id;
     }
 
-
 }
 
 
--- src/test/java/com/example/tsi/izzy/demotest/LanguageTest.java
+++ src/test/java/com/example/tsi/izzy/demotest/LanguageTest.java
@@ -1,18 +1,29 @@
 package com.example.tsi.izzy.demotest;
 
+import com.example.tsi.izzy.demo.Language;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LanguageTest {
 
     @Test
     void getNameTest() {
+        Language getNameTest = new Language("testName");
+        assertEquals("testName",getNameTest.getName(),"This test has failed");
     }
 
     @Test
     void setNameTest() {
+        Language getNameTest = new Language("testName");
+        getNameTest.setName("testNameChange");
+        assertEquals("testNameChange",getNameTest.getName(),"This test has failed");
+
     }
 
     @Test
     void getLanguage_idTest() {
+        Language getNameTest = new Language("testName");
+        assertEquals(0,getNameTest.getLanguage_id(),"This test has failed");
     }
 }
